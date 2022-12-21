959d736262e69b5acb849ac35418ab3a39c43e2d
Hadar Neulander <hadarnoylander@gmail.com>
2022-12-21 20:35:00 +0200

    Update network.tf

--- us-west-2/network.tf
+++ us-west-2/network.tf
@@ -14,51 +14,51 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
-resource "aws_subnet" "private-us-west-1a" {
+resource "aws_subnet" "private-us-west-2a" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.0.0/19"
-  availability_zone = "us-west-1a"
+  availability_zone = "us-west-2a"
 
   tags = {
-    "Name"                            = "private-us-west-1a"
+    "Name"                            = "private-us-west-2a"
     "kubernetes.io/role/internal-elb" = "1"
     "kubernetes.io/cluster/demo"      = "owned"
   }
 }
 
-resource "aws_subnet" "private-us-west-1b" {
+resource "aws_subnet" "private-us-west-2b" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.32.0/19"
-  availability_zone = "us-west-1b"
+  availability_zone = "us-west-2b"
 
   tags = {
-    "Name"                            = "private-us-west-1b"
+    "Name"                            = "private-us-west-2b"
     "kubernetes.io/role/internal-elb" = "1"
     "kubernetes.io/cluster/demo"      = "owned"
   }
 }
 
-resource "aws_subnet" "public-us-west-1a" {
+resource "aws_subnet" "public-us-west-2a" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.64.0/19"
-  availability_zone       = "us-west-1a"
+  availability_zone       = "us-west-2a"
   map_public_ip_on_launch = true
 
   tags = {
-    "Name"                       = "public-us-west-1a"
+    "Name"                       = "public-us-west-2a"
     "kubernetes.io/role/elb"     = "1"
     "kubernetes.io/cluster/demo" = "owned"
   }
 }
 
-resource "aws_subnet" "public-us-west-1b" {
+resource "aws_subnet" "public-us-west-2b" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.96.0/19"
-  availability_zone       = "us-west-1b"
+  availability_zone       = "us-west-2b"
   map_public_ip_on_launch = true
 
   tags = {
-    "Name"                       = "public-us-west-1b"
+    "Name"                       = "public-us-west-2b"
     "kubernetes.io/role/elb"     = "1"
     "kubernetes.io/cluster/demo" = "owned"
   }
@@ -74,7 +74,7 @@ resource "aws_eip" "nat" {
 
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public-us-west-1a.id
+  subnet_id     = aws_subnet.public-us-west-2a.id
 
   tags = {
     Name = "nat"
